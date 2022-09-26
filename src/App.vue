a5ebe037bda4283fb9b6becdba638fec09091a91
xiyuan <547839840@qq.com>
2022-09-26 18:40:44 +0800

    侧边栏开发中

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
-      <div class="sidebar">
-          
+      <div :class="index ? 'sidebar':'sidebar2'">
       </div>
-      <transition name="fade">
-          <router-view/>
-      </transition>
+      <div :class="index ? 'anniu':'anniu2'">
+            <i v-if="index==true" @click="index=!index" class="el-icon-caret-left"></i>
+            <i v-else @click="index=!index" class="el-icon-caret-right"></i>
+      </div>
+      <div :class="index ? 'bodu':'bodu2'">
+        <transition name="fade">
+            <router-view/>
+        </transition>
+      </div>
   </div>
 </template>
 
@@ -13,7 +18,7 @@
    export default {
    data() {
        return {
-        
+          index:true
        }
    },
    methods:{
@@ -32,9 +37,9 @@ body {
   border:0;
 }
 .sidebar{
-  
+  background: #000;
   position: fixed;
-  width: 50px;
+  width: 240px;
   height: 100vh;
   transition: all .4s;
   float: left;
@@ -42,8 +47,15 @@ body {
   z-index: 200;
   
 }
-.sidebar:hover{
-  width: 240px;
+.sidebar2{
+  background: #000;
+  position: fixed;
+  width: 0px;
+  height: 100vh;
+  transition: all .4s;
+  float: left;
+  overflow: hidden;
+  z-index: 200;
 }
 
 
@@ -53,6 +65,43 @@ body {
 .fade-enter {
   opacity: 0;
   transform: translateX(40px);
+}
+.bodu{
+  margin-left: 240px;
+  transition: all .4s;
+}
+.bodu2{
+  margin-left: 0px;
+  transition: all .4s;
+}
+.anniu{
+  position: fixed;
+  z-index: 990;
+  float: left;
+  margin-left: 230px;
+  font-size: 40px;
+  line-height: 100vh;
+  transition: all .4s;
+}
+.anniu2{
+  position: fixed;
+  z-index: 990;
+  float: left;
+  margin-left: -10px;
+  font-size: 40px;
+  line-height: 100vh;
+  transition: all .4s;
+}
+.el-icon-caret-left,.el-icon-caret-right{
+  color: rgb(209, 209, 209);
+}
+.el-icon-caret-left:hover{
+  color: rgb(28, 183, 255);
+  cursor:pointer;
+}
+.el-icon-caret-right:hover{
+  color: rgb(28, 183, 255);
+  cursor:pointer;
 }
 </style>
 
